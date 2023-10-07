016786fc4b3357e9d14b429c64bea7966b56fe4d
kaito01234 <43839796+kaito01234@users.noreply.github.com>
2023-10-07 07:27:40 +0000

    update devcontainer

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# set alias
+echo 'alias ll="ls -alF"' >> ~/.bash_aliases
+echo 'alias d="docker"' >> ~/.bash_aliases
+echo 'alias dc="docker compose"' >> ~/.bash_aliases
+
+# setup volta
+volta setup
